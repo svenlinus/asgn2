829fa3c02ccb9daf27c4ff4cc8e19868e5226f66
thekevinWHYuan <kev.yuan01@gmail.com>
2025-04-22 00:12:28 -0700

    even more setup

--- lwp.c
+++ lwp.c
@@ -1,4 +1,27 @@
+tid_t lwp_create(lwpfun, void *){
 
-int main() {
-  return 0;
+}
+void  lwp_exit(int status){
+
+}
+tid_t lwp_gettid(void){
+
+}
+void  lwp_yield(void){
+
+}
+void  lwp_start(void){
+
+}
+tid_t lwp_wait(int *){
+
+}
+void  lwp_set_scheduler(scheduler fun){
+
+}
+scheduler lwp_get_scheduler(void){
+
+}
+thread tid2thread(tid_t tid){
+
 }
---
+++ scheduler.c
@@ -0,0 +1,19 @@
+void init(void){
+
+}
+
+void shutdown(void){
+
+}
+
+void admit(thread new){
+
+}
+
+void remove(thread victim){
+
+}
+
+int qlen(void){
+
+}
