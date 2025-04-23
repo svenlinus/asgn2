3aafed746858930ce25907b17987dd4457700cf1
linustoernqvist <linus.tornqvist@triggerbee.com>
2025-04-23 15:02:04 -0700

    ready for test prob

--- lwp.c
+++ lwp.c
@@ -1,3 +1,6 @@
+/* TODO: Wrap */
+
+#include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <sys/mman.h>
@@ -13,6 +16,10 @@ int mmap_flags = MAP_PRIVATE | MAP_ANONYMOUS;
     mmap_flags |= MAP_STACK;
 #endif
 int tid_incr = 0;
+thread curr_thread = NULL;
+thread exited_threads = NULL;
+thread waiting_threads = NULL;
+thread all_threads = NULL;
 
 /* Rounds up to the nearest multiple of 16 greater than `x` */
 uintptr_t roundUp(uintptr_t x, int multiple) {
@@ -57,24 +64,37 @@ tid_t lwp_create(lwpfun function, void *argument) {
   /* Because stacks grow downward, compute the top of the allocated memory */
   /* This pointer will be the base of the stack */
   uintptr_t base_ptr = stack_top + stack_size;
+  /* Add return address to stack so that the program jumps to the function */
+  ((unsigned char *)base_ptr)[1] = function;
+  ((unsigned char *)base_ptr)[2] = base_ptr;
+
   /* Allocate a context for the lwp */
   thread lwp = (thread)calloc(1, sizeof(context));
   if (lwp == NULL) {
-    perror("malloc");
+    perror("calloc");
     return NO_THREAD;
   }
   lwp->stack = base_ptr;
   lwp->stacksize = stack_size;
   lwp->tid = tid_incr++;
+  lwp->status = MKTERMSTAT(LWP_LIVE, 0);
   lwp->state.fxsave = FPU_INIT;
-  /* Set base pointer to function to return to this thread */
-  lwp->state.rbp = (uintptr_t)function - 1;
+  /* Set base pointer to this threads stack */
+  lwp->state.rbp = base_ptr + 2;
   /* In x86, the first function argument is stored in rdi */
   lwp->state.rdi = (uintptr_t)argument;
 
   /* Add lwp to scheduler */
   scheduler sched = lwp_get_scheduler();
   sched->admit(lwp);
+
+  /* Manage datastructure for gettid (lib_two) */
+  lwp->lib_two = all_threads;
+  all_threads = lwp;
+  
+  if (curr_thread == NULL) {
+    curr_thread = lwp;
+  }
 
   return lwp->tid;
 }
@@ -130,7 +150,108 @@ void lwp_start() {
 }
 
 void lwp_yield() {
-  
+  if (curr_thread == NULL) {
+    fprintf(STDERR_FILENO, "Can't yield with no threads running\n");
+    return;
+  } 
+  /* Get the next thread to run */
+  scheduler sched = lwp_get_scheduler();
+  thread next = sched->next();
+  if (next == NULL) {
+    exit(curr_thread->status);
+  }
+  /* Save current context and load next */
+  rfile curr_state;
+  swap_rfiles(&curr_state, &(next->state));
+  curr_thread->state = curr_state;
+  curr_thread = next;
+}
+
+
+void lwp_exit(int status) {
+  if (curr_thread == NULL) {
+    fprintf(STDERR_FILENO, "Can't exit with no threads running\n");
+    return;
+  }
+  scheduler sched = lwp_get_scheduler();
+  int exit_code = LWPTERMSTAT(status);
+  curr_thread->status = MKTERMSTAT(LWP_TERM, exit_code);
+  /* Check for waiting threads */
+  if (waiting_threads != NULL) {
+    /* Reschedule waiting thread and associate with it */
+    waiting_threads->exited = curr_thread;
+    sched->admit(waiting_threads);
+    /* Deque waiting thread */
+    waiting_threads = waiting_threads->lib_one;
+    /* Remove curr thread from schedule */
+    sched->remove(curr_thread);
+    return;
+  }
+
+  /* Add curr_thread to end of exited threads list */
+  if (exited_threads == NULL) {
+    exited_threads = curr_thread;
+  }
+  else {
+    thread iter_thread = exited_threads;
+    while (iter_thread->exited) {
+      iter_thread = iter_thread->exited;
+    }
+    iter_thread->exited = curr_thread;
+  }
+  /* Remove curr thread from schedule */
+  sched->remove(curr_thread);
+}
+
+tid_t lwp_wait(int *status) {
+  if (curr_thread == NULL) {
+    fprintf(STDERR_FILENO, "Can't wait with no threads running\n");
+    return;
+  }
+  scheduler sched = lwp_get_scheduler();
+  if (sched->qlen() <= 1) {
+    return NO_THREAD;
+  } 
+  thread exit_thread;
+  /* Check for exited threads */
+  if (exited_threads != NULL) {
+    /* Deque the oldest exited thread (head) */
+    exit_thread = exited_threads;
+    exited_threads = exited_threads->exited;
+  }
+  else {
+    /* Add curr_thread to end of waiting list */
+    if (waiting_threads == NULL) {
+      waiting_threads = curr_thread;
+    }
+    else {
+      thread iter_thread = waiting_threads;
+      while (iter_thread->lib_one) {
+        iter_thread = iter_thread->lib_one;
+      }
+      iter_thread->lib_one = curr_thread;
+    }
+    /* Block (deschedule and yield) */
+    sched->remove(curr_thread);
+    curr_thread->exited = NULL;
+    lwp_yield();
+
+    /* Thread rescheduled */
+    exit_thread = curr_thread->exited;
+    if (exit_thread == NULL) {
+      fprintf(STDERR_FILENO, "Thread rescheduled with no associated exits\n");
+      return NO_THREAD;
+    }
+  }
+  tid_t tid = exit_thread->tid;
+  *status = exit_thread->status ? LWPTERMSTAT(exit_thread->status) : NULL;
+  /* Deallocate exited thread */
+  if (exit_thread->stack) {
+    free(exit_thread->stack);
+  }
+  free(exit_thread);
+
+  return tid;
 }
 
 int main() {
