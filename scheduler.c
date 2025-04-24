c67fd83d385e5bdaedc0bd310275a9185ce58b86
Kevin <kev.yuan01@gmail.com>
2025-04-23 20:51:27 -0700

    changed scheduler name

--- scheduler.c
+++ scheduler.c
@@ -75,5 +75,5 @@ int qlen(void){
 }
 
 
-struct scheduler round_robin_scheduler = {init, shutdown, admit, remove, next, qlen};
-struct scheduler current_scheduler = {init, shutdown, admit, remove, next, qlen};
+struct scheduler round_robin_sched = {init, shutdown, admit, remove, next, qlen};
+struct scheduler current_sched = {init, shutdown, admit, remove, next, qlen};
